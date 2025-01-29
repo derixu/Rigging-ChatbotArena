aca8574b8dcd1b4a48bf3fe58ac36027decf6685
rmin <rmin2000aa@gmail.com>
2025-01-30 00:44:59 +0800

    update

--- classifier/train.py
+++ classifier/train.py
@@ -27,7 +27,7 @@ class TextDataset(Dataset):
         label = torch.tensor(self.dataset_list[idx][1])
         return {"input_ids": input, "token_type_ids": torch.zeros_like(input), "labels": label}
 
-json_list = ['Llama-2-7b-chat-hf', 'Llama-2-13b-chat-hf', 'Meta-Llama-3-8B-Instruct', 'c4ai-command-r-v01', 'gpt-4o-mini-2024-07-18',\
+model_name_list = ['Llama-2-7b-chat-hf', 'Llama-2-13b-chat-hf', 'Meta-Llama-3-8B-Instruct', 'c4ai-command-r-v01', 'gpt-4o-mini-2024-07-18',\
                  'Mistral-7B-Instruct-v0.2', 'Mistral-7B-Instruct-v0.1', \
                   'gemma-2-27b-it', 'gemma-2b-it', 'gemma-2-9b-it',\
                      'Qwen1.5-7B-Chat', 'Qwen1.5-14B-Chat', 'Starling-LM-7B-alpha', 'Starling-LM-7B-beta', 'Yi-34B-Chat','Yi-1.5-34B-Chat',\
@@ -35,20 +35,15 @@ json_list = ['Llama-2-7b-chat-hf', 'Llama-2-13b-chat-hf', 'Meta-Llama-3-8B-Instr
                             'vicuna-7b-v1.3', 'mpt-7b-chat', 'openchat_3.5', 'WizardLM-13B-V1.2', 'SOLAR-10.7B-Instruct-v1.0']
 
 parser = argparse.ArgumentParser('dl detector')
-parser.add_argument(
-    '-i', '--input', type=str, help='input file path',
-    default='zh/mix'
-)
-parser.add_argument(
-    '-m', '--model-name', type=str, help='model name', default='FacebookAI/roberta-base'
-)
+
+parser.add_argument('--model-name', type=str, help='model name', default='FacebookAI/roberta-base')
 parser.add_argument('--batch_size', type=int, default=64, help='batch size')
-parser.add_argument('-e', '--epochs', type=int, default=20, help='batch size')
-parser.add_argument('--cuda', '-c', type=str, default='0', help='gpu ids, like: 1,2,3')
-parser.add_argument('--seed', '-s', type=int, default=42, help='random seed.')
+parser.add_argument('--epochs', type=int, default=20, help='batch size')
+
+parser.add_argument('--seed', type=int, default=2025, help='random seed.')
 parser.add_argument('--max-length', type=int, default=512, help='max_length')
 parser.add_argument("--dataset", default='hc3')
-parser.add_argument("--all-train", action="store_true", default=False, help='use all data for training')
+
 parser.add_argument('--eval_freq', type=int, default=5000, help='eval frequency')
 parser.add_argument('--data_size', type=int, default=4000)
 parser.add_argument('--eval_size', type=int, default=1000)
@@ -56,22 +51,15 @@ args = parser.parse_args()
 
 
 
-
-
-
-
 def main(args: argparse.Namespace):
     
-   
-    
-
     random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed_all(args.seed)
 
 
-    print('Class Num: ', len(json_list))
+    print('Class Num: ', len(model_name_list))
     train_list = []
     test_list = []
     kwargs = dict(max_length=args.max_length, truncation=True)
@@ -79,7 +67,7 @@ def main(args: argparse.Namespace):
     
    
     
-    for model_id in json_list:
+    for model_id in model_name_list:
        
         file_name = f'./training_data_classifier/{args.dataset}/{model_id}.json'
         with open(file_name) as f:
@@ -93,10 +81,10 @@ def main(args: argparse.Namespace):
                 tokenized_response = tokenizer.encode(response)
                 if len(tokenized_response) < 100:
                     continue
-                train_list.append((tokenized_response[:args.max_length], json_list.index(model_id)))
+                train_list.append((tokenized_response[:args.max_length], model_name_list.index(model_id)))
     
 
-    for model_id in json_list:
+    for model_id in model_name_list:
         file_name = f'./training_data_classifier/{args.dataset}/{model_id}.json'
         with open(file_name) as f:
             data = json.load(f)
@@ -109,7 +97,7 @@ def main(args: argparse.Namespace):
                 tokenized_response = tokenizer.encode(response)
                 if len(tokenized_response) < 100:
                     continue
-                test_list.append((tokenized_response[:args.max_length], json_list.index(model_id)))
+                test_list.append((tokenized_response[:args.max_length], model_name_list.index(model_id)))
 
 
     
@@ -124,7 +112,7 @@ def main(args: argparse.Namespace):
         predictions = np.argmax(logits, axis=-1)
         return accuracy.compute(predictions=predictions, references=labels)
 
-    model = AutoModelForSequenceClassification.from_pretrained(args.model_name, num_labels=len(json_list), device_map='auto')
+    model = AutoModelForSequenceClassification.from_pretrained(args.model_name, num_labels=len(model_name_list), device_map='auto')
 
     output_dir = f"./models/{args.dataset}/class_{args.data_size}/"  # checkpoint save path
   
